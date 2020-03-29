a681babc99fbe7546f085e5d48fbbf1ad002086a
leoprananta <leonantapramudya7@gmail.com>
2020-03-29 23:06:11 +0700

    finishing

--- js/api.js
+++ js/api.js
@@ -1,40 +1,114 @@
 getCovid();
+getProv();
 
 function getCovid(){
 
 	$('#list').html('');
 
 	$.ajax({
-		url: 'http://corona.lmao.ninja/countries/USA',
-    type: 'GET',
-    dataType: 'jsonp',
+		url: 'http://indonesia-covid-19.mathdro.id/api/',
+    	type: 'GET',
+    	dataType: 'json',
 		success: function(result){
-      console.log(result);
-			// if(result.Response == "True"){
+		
+			console.log(result);
 
-      //   let covid = result
+			if(result == null){
+				$('#data1').html(
+					`<p>Loading..</p>`
+				)
+			}else{
+				$('#data1').append(`
 
-			// 	$.each(covid, function(i, data){
-      //     $('#list').append(`
+		  	</div>
+				<div class="col-md-6">
+					<div class="card mb-3 p-2 sr-icon-3">
+						<div class="card-body">
+							<h1 class="text-center text-primary"><strong>`+ result.jumlahKasus +`</strong></h1>
+							<p class="card-text text-center">Jumlah Kasus</p>
+						</div>
+					</div>
+				</div>
+			</div>
 
-      //       <div class="col-md-4">
-      //         <div class="card mb-3 p-2 sr-icon-2">
-      //           <div class="card-body">
-      //             <h5 class="card-title">`+ data.country +`</h5>
-      //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      //             <a href="#" class="btn btn-info">More Details...</a>
-      //           </div>
-      //         </div>
-      //       </div>
-			// 			`);
-			// 	});
-			// }else{
-			// 	$('#list').html(`
-			// 		<div class="col">
-			// 		<h1 class="text-center">`+ result.Error +`</h1>
-			// 		</div>
-			// 		`)
-			// }
+			</div>
+				<div class="col-md-6">
+					<div class="card mb-3 p-2 sr-icon-3">
+						<div class="card-body">
+							<h1 class="text-center text-warning"><strong>`+ result.perawatan +`</strong></h1>
+							<p class="card-text text-center">Dirawat</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			</div>
+				<div class="col-md-6">
+					<div class="card mb-3 p-2 sr-icon-3">
+						<div class="card-body">
+							<h1 class="text-center text-success"><strong>`+ result.sembuh +`</strong></h1>
+							<p class="card-text text-center">Sembuh</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			</div>
+				<div class="col-md-6">
+					<div class="card mb-3 p-2 sr-icon-3">
+						<div class="card-body">
+							<h1 class="text-center text-danger"><strong>`+ result.meninggal +`</strong></h1>
+							<p class="card-text text-center">Meninggal</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			`);
+			}
+		}
+	});
+}
+
+function getProv(){
+	$.ajax({
+		url: 'http://indonesia-covid-19.mathdro.id/api/provinsi',
+    	type: 'GET',
+    	dataType: 'json',
+		success: function(result){
+			console.log(result);
+			let provinsi = result.data
+
+			if(provinsi == null){
+				$('#dataProv').html(
+					`<p>Loading..</p>`
+				)
+			}else{
+				$.each(provinsi, function(i, data){
+					$('#dataProv').append(`
+					<div class="col-md-4">
+						<div class="card mb-4 p-2 sr-icon-1">
+						<div class="card-body">
+							<h5 class="card-title text-center"><i class="fas fa-map-pin"></i><strong> `+ data.provinsi +`</strong></h5>
+							<div class="row mt-4">
+								<div class="col">
+									<h5 class="card-text text-center text-primary"><strong>`+ data.kasusPosi +`</strong></h5>
+									<p class="text-center">Positif</p>
+								</div>
+								<div class="col">
+									<h5 class="card-text text-center text-success"><strong>`+ data.kasusSemb +`</strong></h5>
+									<p class="text-center">Sembuh</p>
+								</div>
+								<div class="col">
+									<h5 class="card-text text-center text-danger"><strong>`+ data.kasusMeni +`</strong></h5>
+									<p class="text-center">Meninggal</p>
+								</div>
+							</div>
+						</div>
+						</div>
+					</div>
+						`);
+				});
+			}
 		}
 	});
 }
